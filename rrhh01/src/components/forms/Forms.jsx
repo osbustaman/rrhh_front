ec764df56e5f8f42fa0d77466874d6769ce955c7
osbustaman4 <osvaldo.bustamante.n@gmail.com>
2024-07-10 16:34:50 -0400

    se crea el componente para crear formularios, por ahora solo select e inputs text, number y select autocompletable

--- rrhh01/src/components/forms/Forms.jsx
+++ rrhh01/src/components/forms/Forms.jsx
@@ -1,42 +1,13 @@
 
 import { InputText } from './InputText';
-import { InputTextLabelUp } from './InputTextLabelUp';
 
-export const Forms = () => {
-
-    const config_form = {
-        id: 'demo-form2',
-        data_parsley_validate: '',
-        class_name: '',
-        novalidate: '',
-        columns: 6,
-        inputs: [
-            {
-                label: 'Nombre cliente',
-                required: true,
-                type: 'text',
-                id: 'first-name',
-                name: 'first-name',
-                number_row: 6
-            }
-        ]
-    };
-
-    <form id="demo-form" data-parsley-validate="" novalidate=""></form>
-    console.log(config_form.inputs[0].number_row);
+export const Forms = ({config_form}) => {
 
     return (
         <>
-            <form id={config_form.id}>
-                <div className="row">
-
-                    <InputText config_input={config_form.inputs[0]}/>
-                    
-                </div>
-                
-                
-                
-            </form>
+            <div className="row">
+                <InputText config_input={config_form}/>
+            </div>
         </>
     )
 }
--- rrhh01/src/components/forms/InputText.jsx
+++ rrhh01/src/components/forms/InputText.jsx
@@ -1,17 +1,107 @@
+import { actionMap } from '../../js/ validations';
+import Select from 'react-select'
+
 
 export const InputText = ({ config_input }) => {
 
-    const { label, number_row, required, type, id, name } = config_input;
+    const {position_form, number_row} = config_input;
+
+    const renderInput = (key) => {
+
+        // Función para manejar eventos de manera genérica
+        const handleEvent = (key, e) => {
+
+            if (e.target.value.trim() === '') {
+                return;
+            }
+
+            if (key.evento && actionMap[key.evento.action]) {
+                // Ejecuta la acción correspondiente
+                const isValid = actionMap[key.evento.action](e.target.value);
+
+                $.alert({
+                    title: 'Error!',
+                    content: isValid ? key.evento.message.success : key.evento.message.error,
+                });
+                return;
+            }
+        };
+
+        // Prepara el objeto de propiedades del evento de manera condicional
+        const eventProps = key.evento ? { [key.evento.name]: (e) => handleEvent(key, e) } : {};
+
+
+        switch (key.type) {
+            case "select":
+                return (
+                    <select id={key.id} className="form-control" name={key.label.toLowerCase().replace(/ /g, "_")} required={!!key.required}>
+                        <option>{key.text_default}</option>
+                        {key.options.map((option) => (
+                            <option key={option.value} value={option.value}>{option.label}</option>
+                        ))}
+                    </select>
+                );
+            case "select_autocomplete":
+
+                const handleChange = (selectedOption) => {
+                    //console.log(`Input Name: ${key.label.toLowerCase().replace(/ /g, "_")}, Selected ID: ${selectedOption.value}`);
+                };
+
+                return (
+                    <Select
+                        id={key.id} 
+                        name={key.label.toLowerCase().replace(/ /g, "_")}
+                        options={key.options}
+                        placeholder={key.text_default}
+                        onChange={handleChange}
+                        getOptionLabel={(option) => option.label}
+                        getOptionValue={(option) => option.value} />
+                );
+
+            case "checkbox":
+                return (
+                    <input 
+                        type="checkbox" 
+                        id={key.id} 
+                        className="form-check-input" 
+                        name={key.label.toLowerCase().replace(/ /g, "_")} required={!!key.required} 
+                        // Usa el evento configurado en el JSON, como onBlur, onChange, etc.
+                        {...eventProps}
+                        />
+                );
+            case "date":
+                    return (
+                        <input 
+                            type="date" 
+                            id={key.id} 
+                            className="form-control" 
+                            name={key.label.toLowerCase().replace(/ /g, "_")} required={!!key.required}
+                            {...eventProps}
+                            />
+                    );
+            case "text":
+            default:
+                return (
+                    <input 
+                        type="text" 
+                        id={key.id} 
+                        className="form-control" 
+                        name={key.label.toLowerCase().replace(/ /g, "_")} 
+                        required={!!key.required} 
+                        {...eventProps}
+                        />
+                );
+        }
+    };
 
     return (
         <>
-            <div className={`item form-group mb-2 col-md-${number_row}`}>
-                <div>
-                    <label for={id}>{`${label}`} :</label>
+            {config_input.inputs.map((key) => (
+                <div className={`${position_form === 'vertical' ? '' : 'item form-group mb-2'} mb-2 col-md-${number_row}`}>
+                    <label htmlFor={key.label.toLowerCase().replace(/ /g, "_")}>{`${key.label}`} :</label>
+                    {renderInput(key)}
                 </div>
-                
-                <input type={type} id={id} class="form-control" name={name} required={!!required}></input>
-            </div>
+            ))}
         </>
     )
 }
